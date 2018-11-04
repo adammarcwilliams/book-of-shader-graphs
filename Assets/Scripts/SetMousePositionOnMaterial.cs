1fb6fb7a8b7d14124024ff550b6e40fb06a427b6
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2018-11-04 14:46:56 +0000

    Completes Chapter 3

---
+++ Assets/Scripts/SetMousePositionOnMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMousePositionOnMaterial : MonoBehaviour {
+
+    Material material;
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.mousePosition);
+        material.SetVector("_mouse", Input.mousePosition);
+	}
+}
