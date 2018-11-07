07735313960df2563108e5f9381e5001e850aee4
Adam Marc Williams <adam@adammarcwilliams>
2018-11-07 14:05:46 +0000

    Adds Sine Out node

---
+++ Assets/Scripts/Nodes/Easing/SineOutNode.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor.ShaderGraph;
+using System.Reflection;
+
+[Title("Eases", "Sine Out")]
+public class SineOutNode : CodeFunctionNode {
+
+    public SineOutNode()
+    {
+        name = "Sine Out";
+    }
+
+    protected override MethodInfo GetFunctionToConvert()
+    {
+        return GetType().GetMethod("SineOut",
+            BindingFlags.Static | BindingFlags.NonPublic);
+    }
+
+    static string SineOut(
+        [Slot(0, Binding.None)] DynamicDimensionVector A,
+        [Slot(1, Binding.None)] out DynamicDimensionVector Out)
+    {
+        return
+            @"
+{
+    Out = sin(A * 1.57079632f);
+}
+";
+    }
+}
